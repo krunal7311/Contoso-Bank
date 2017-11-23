f9a7b57e40527b0311a56ff586b06e574eb033a2
krunal <krunalbodiwala7311@icloud.com>
2017-11-24 12:55:58 +1300

    connecting remotely
    Registered a bot and added appID and appPassword for remote connectivity

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "1afb6c32-3208-414d-ac86-0e79a3ec614a",
+    appPassword: "bhKVAN691{lfmkgQME10@-?"
 });
 
 // Listen for messages from users 
