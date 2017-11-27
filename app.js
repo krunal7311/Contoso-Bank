8c6ff72d2426ed6f5fd25db1b849acb4be5e36c5
krunal <krunalbodiwala7311@icloud.com>
2017-11-27 13:53:38 +1300

    updaye id and password

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: '0b6817fd-886c-4d11-a3d4-963fb0e66859',
-    appPassword: 'tyuemCN49:%#lzEGXCS745]'
+    appId: "0b6817fd-886c-4d11-a3d4-963fb0e66859",
+    appPassword: "tyuemCN49:%#lzEGXCS745]"
 });
 
 // Listen for messages from users 
