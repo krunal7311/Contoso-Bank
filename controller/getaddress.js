4cd022a1ca5c83db5aabdc111edc2ee7abcb9fd2
krunal <krunalbodiwala7311@icloud.com>
2017-11-27 20:25:10 +1300

    Updated Address

--- controller/getaddress.js
+++ controller/getaddress.js
@@ -10,18 +10,24 @@ function handleAddressResponse(message, session, area)
 {
     var addressResponse = JSON.parse(message);
     var allAddress = []; 
+    var allPhoneNumbers = [];
     for (var index in addressResponse) {
         var usernameReceived = addressResponse[index].area;
         var address = addressResponse[index].address;
+        var phoneNumbers = addressResponse[index].phonenumber;
      if (area.toLowerCase() === usernameReceived.toLowerCase()) {
             //Add a comma after all addresses unless last one
            if(addressResponse.length - 1) {
                 allAddress.push(address);
+                allPhoneNumbers.push(phoneNumbers);
             }
             else {
                 allAddress.push(address + ', ');
+                allPhoneNumbers.push(phoneNumbers + ', ');
+                
             }
        }
 }
 session.send("Nearest branch to  %s is: %s", area, allAddress); 
+session.endDialog("Phone number: %s",allPhoneNumbers);
 }
