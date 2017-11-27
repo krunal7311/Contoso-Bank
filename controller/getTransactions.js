af671d2a2c5d8cdc6aa88f5679e262977afd6252
krunal <krunalbodiwala7311@icloud.com>
2017-11-27 19:15:29 +1300

    trial41

--- controller/getTransactions.js
+++ controller/getTransactions.js
@@ -2,7 +2,7 @@ var rest = require('../API/RestClient');
 var builder = require('botbuilder');
 var table = module.exports = require('azure-mobile-apps').table();
 exports.displayTransactions = function getTransactions(session, user){
-    var url = 'http://contosotables.azurewebsites.net/tables/contosoTransactions';
+    var url = 'http://kcontoso.azurewebsites.net/tables/contosoTransactions';
     rest.getAccount(url, session, user, handleTransactionResponse)
 };
 
